98a45930993a09f669ae60111df01d4ceb1838cf
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-03-11 09:05:39 +0100

    TEST-NO MERGE

--- ct/debian.sh
+++ ct/debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
-# Copyright (c) 2021-2025 tteck
+# Copyright (c) 2021-2025 ttec
 # Author: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://www.debian.org/
