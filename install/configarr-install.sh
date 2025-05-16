3a0bfbeada2f9a4c45a561db8e198a71f8cde81f
finkerle <145992792+finkerle@users.noreply.github.com>
2025-05-16 11:30:14 +0200

    Update install/configarr-install.sh
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>

--- install/configarr-install.sh
+++ install/configarr-install.sh
@@ -18,17 +18,7 @@ $STD apt-get install -y \
     git
 msg_ok "Installed Dependencies"
 
-msg_info "Setting up Node.js Repository"
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-msg_ok "Set up Node.js Repository"
-
-msg_info "Installing Node.js"
-$STD apt-get update
-$STD apt-get install -y nodejs
-$STD npm install -g pnpm
-msg_ok "Installed Node.js"
+NODE_MODULE="pnpm@latest" install_node_and_modules
 
 msg_info "Installing Configarr"
 
