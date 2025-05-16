bc52789667ab7d9257f230e1df6d6f4a71057178
finkerle <145992792+finkerle@users.noreply.github.com>
2025-05-16 11:56:55 +0200

    Update ct/configarr.sh
    Co-authored-by: Slaviša Arežina <58952836+tremor021@users.noreply.github.com>

--- ct/configarr.sh
+++ ct/configarr.sh
@@ -29,7 +29,6 @@ function update_script() {
     fi
     RELEASE=$(curl -fsSL https://api.github.com/repos/raydak-labs/configarr/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
     if [[ "${RELEASE}" != "$(cat /opt/configarr_version.txt)" ]] || [[ ! -f /opt/configarr_version.txt ]]; then
-
         msg_info "Stopping $APP"
         systemctl stop configarr-task.timer
         msg_ok "Stopped $APP"
