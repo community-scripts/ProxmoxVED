db9cf2f438fecbf65791a3811e16ccb1e8e1d763
Kellan Stevens <kellan@kellanstevens.com>
2025-06-07 23:38:17 +0200

    chore(ct/stremio.sh): let's try ubuntu

--- ct/stremio.sh
+++ ct/stremio.sh
@@ -7,11 +7,11 @@ source <(curl -fsSL https://raw.githubusercontent.com/KellanStevens/ProxmoxVED/s
 
 APP="Stremio"
 var_tags="${var_tags:-media;stremio}"
-var_disk="${var_disk:-6}"
+var_disk="${var_disk:-8}"
 var_cpu="${var_cpu:-1}"
-var_ram="${var_ram:-2048}"
-var_os="${var_os:-debian}"
-var_version="${var_version:-12}"
+var_ram="${var_ram:-1024}"
+var_os="${var_os:-ubuntu}"
+var_version="${var_version:-24.10}"
 var_unprivileged="${var_unprivileged:-1}"
 
 header_info "$APP"
@@ -20,15 +20,15 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
-    if [[ ! -d /opt/stremio ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    msg_error "Currently we don't provide an update function for this ${APP}."
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/stremio ]]; then
+    msg_error "No ${APP} Installation Found!"
     exit
+  fi
+  msg_error "Currently we don't provide an update function for this ${APP}."
+  exit
 }
 
 start
