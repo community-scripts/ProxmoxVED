08e3a663642c848105512ba7f71ca3ea6a82a185
Ben Whybrow <stentorianbusiness@gmail.com>
2025-06-25 13:50:49 +0000

    Change references for local testing

--- ct/opencut.sh
+++ ct/opencut.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/refs/heads/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/bwhybrow23/ProxmoxVED/refs/heads/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: Ben Whybrow
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
