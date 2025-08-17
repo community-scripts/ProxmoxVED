83137fb7489a2e6d3f57264f9c8bd6a147782204
Cil <sto1nks@yahoo.com>
2025-08-17 16:54:47 -0500

    Prepare for pull request

--- install/otterwiki-install.sh
+++ install/otterwiki-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: CillyCil
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/redimp/otterwiki
 
 # Import Functions und Setup
