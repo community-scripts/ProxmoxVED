8dbc48616daa7d8324d815307d3b46d15fad0e0a
DESKTOP-UULMCR6\Joshua <JoshuaShabanov@gmail.com>
2025-08-17 23:42:25 +0300

    Make install silent

--- install/overleaf-install.sh
+++ install/overleaf-install.sh
@@ -14,27 +14,27 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-apt update
-apt install -y git ca-certificates curl gnupg
+apt -qq update >/dev/null
+$STD apt -qq install -y git ca-certificates curl gnupg
 install -d -m 0755 /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" >/etc/apt/sources.list.d/docker.list
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-systemctl enable --now docker
+apt -qq update >/dev/null
+$STD apt -qq install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+$STD systemctl enable --now docker
 msg_ok "Installed Dependencies"
 
 msg_info "Setup Overleaf"
 install -d -m 0755 /opt
 if [[ -d /opt/overleaf-toolkit/.git ]]; then
-  git -C /opt/overleaf-toolkit pull -q
+  $STD git -C /opt/overleaf-toolkit pull -q
 else
-  git clone -q https://github.com/overleaf/toolkit.git /opt/overleaf-toolkit
+  $STD git clone -q https://github.com/overleaf/toolkit.git /opt/overleaf-toolkit
 fi
 cd /opt/overleaf-toolkit
 
-bin/init </dev/null
+$STD bin/init </dev/null
 
 CFG="config/overleaf.rc"
 grep -q '^OVERLEAF_LISTEN_IP=' "$CFG" \
@@ -45,7 +45,7 @@ grep -q '^SIBLING_CONTAINERS_ENABLED=' "$CFG" \
   && sed -i 's/^SIBLING_CONTAINERS_ENABLED=.*/SIBLING_CONTAINERS_ENABLED=false/' "$CFG" \
   || echo 'SIBLING_CONTAINERS_ENABLED=false' >> "$CFG"
 
-bin/up -d
+$STD bin/up -d
 IPV4="$(hostname -I | awk '{print $1}')"
 echo "Overleaf is starting. URL: http://${IPV4}"
 echo "First step: create the admin user at http://${IPV4}/launchpad"
@@ -56,6 +56,6 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-apt -y autoremove
-apt -y autoclean
+$STD apt -y autoremove
+$STD apt -y autoclean
 msg_ok "Cleaned"
