b245c3fb7dfc60192b0005e6d7c63d4b33649344
dellthePROgrammer <delpierobatou@gmail.com>
2025-09-05 10:07:12 -0400

    ensure install script is run

--- ct/traefik.sh
+++ ct/traefik.sh
@@ -47,6 +47,9 @@ start
 build_container
 description
 
+msg_info "Installing ${APP} LXC"
+pct exec $CTID -- bash -c "curl -fsSL https://raw.githubusercontent.com/dellthePROgrammer/ProxmoxVED/main/install/traefik-install.sh | bash"
+
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
