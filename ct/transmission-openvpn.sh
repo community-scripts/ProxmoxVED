375909e52133620b4b477d2c289b062921d2b840
SunFlowerOwl <drv.emeric@gmail.com>
2025-10-09 13:33:27 -0400

    CR, OS: Migrate to Debian 13

--- ct/transmission-openvpn.sh
+++ ct/transmission-openvpn.sh
@@ -11,7 +11,7 @@ var_cpu="${var_cpu:-1}"
 var_ram="${var_ram:-512}"
 var_disk="${var_disk:-8}"
 var_os="${var_os:-debian}"
-var_version="${var_version:-12}"
+var_version="${var_version:-13}"
 var_unprivileged="${var_unprivileged:-1}"
 var_tun="${var_tun:-yes}"
 
@@ -21,57 +21,57 @@ color
 catch_errors
 
 function update_script() {
-  header_info
-  check_container_storage
-  check_container_resources
-  if [[ ! -d /opt/transmission-openvpn/ ]]; then
-    msg_error "No ${APP} Installation Found!"
+    header_info
+    check_container_storage
+    check_container_resources
+    if [[ ! -d /opt/transmission-openvpn/ ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+
+    msg_info "Updating Dependencies"
+    $STD apt-get update
+    $STD apt-get -y upgrade
+    msg_ok "Updated Dependencies"
+
+    if check_for_gh_release "docker-transmission-openvpn" "haugene/docker-transmission-openvpn"; then
+        msg_info "Stopping $APP"
+        systemctl stop openvpn-custom
+        msg_ok "Stopped $APP"
+
+        msg_info "Saving Custom Configs"
+        mv /etc/openvpn/custom /opt/transmission-openvpn/
+        rm -f /opt/transmission-openvpn/config-failure.sh
+        msg_ok "Saved Custom Configs"
+
+        msg_info "Updating ${APP} LXC"
+        fetch_and_deploy_gh_release "docker-transmission-openvpn" "haugene/docker-transmission-openvpn" "tarball" "latest" "/opt/docker-transmission-openvpn"
+        rm -rf /etc/openvpn/* /etc/transmission/* /etc/scripts/* /opt/privoxy/*
+        cp -r /opt/docker-transmission-openvpn/openvpn/* /etc/openvpn/
+        cp -r /opt/docker-transmission-openvpn/transmission/* /etc/transmission/
+        cp -r /opt/docker-transmission-openvpn/scripts/* /etc/scripts/
+        cp -r /opt/docker-transmission-openvpn/privoxy/scripts/* /opt/privoxy/
+        chmod +x /etc/openvpn/*.sh
+        chmod +x /etc/scripts/*.sh
+        chmod +x /opt/privoxy/*.sh
+        msg_ok "Updated ${APP} LXC"
+
+        msg_info "Restoring Custom Configs"
+        cp -r /opt/transmission-openvpn/custom/* /etc/openvpn/custom/
+        msg_ok "Restored Custom Configs"
+
+        msg_info "Starting $APP"
+        systemctl start openvpn-custom
+        msg_ok "Started $APP"
+    fi
+
+    msg_info "Cleaning up"
+    $STD apt-get -y autoremove
+    $STD apt-get -y autoclean
+    rm -rf /opt/docker-transmission-openvpn
+    msg_ok "Cleaned"
+
     exit
-  fi
-
-  msg_info "Updating Dependencies"
-  $STD apt-get update
-  $STD apt-get -y upgrade
-  msg_ok "Updated Dependencies"
-
-  if check_for_gh_release "docker-transmission-openvpn" "haugene/docker-transmission-openvpn"; then
-    msg_info "Stopping $APP"
-    systemctl stop openvpn-custom
-    msg_ok "Stopped $APP"
-
-    msg_info "Saving Custom Configs"
-    mv /etc/openvpn/custom /opt/transmission-openvpn/
-    rm -f /opt/transmission-openvpn/config-failure.sh
-    msg_ok "Saved Custom Configs"
-
-    msg_info "Updating ${APP} LXC"
-    fetch_and_deploy_gh_release "docker-transmission-openvpn" "haugene/docker-transmission-openvpn" "tarball" "latest" "/opt/docker-transmission-openvpn"
-    rm -rf /etc/openvpn/* /etc/transmission/* /etc/scripts/* /opt/privoxy/*
-    cp -r /opt/docker-transmission-openvpn/openvpn/* /etc/openvpn/
-    cp -r /opt/docker-transmission-openvpn/transmission/* /etc/transmission/
-    cp -r /opt/docker-transmission-openvpn/scripts/* /etc/scripts/
-    cp -r /opt/docker-transmission-openvpn/privoxy/scripts/* /opt/privoxy/
-    chmod +x /etc/openvpn/*.sh
-    chmod +x /etc/scripts/*.sh
-    chmod +x /opt/privoxy/*.sh
-    msg_ok "Updated ${APP} LXC"
-
-    msg_info "Restoring Custom Configs"
-    cp -r /opt/transmission-openvpn/custom/* /etc/openvpn/custom/
-    msg_ok "Restored Custom Configs"
-
-    msg_info "Starting $APP"
-    systemctl start openvpn-custom
-    msg_ok "Started $APP"
-  fi
-
-  msg_info "Cleaning up"
-  $STD apt-get -y autoremove
-  $STD apt-get -y autoclean
-  rm -rf /opt/docker-transmission-openvpn
-  msg_ok "Cleaned"
-
-  exit
 }
 
 start
