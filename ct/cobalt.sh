38b355bcaa4f603cc8002f373fa30d8622c5e2e6
Kofy <baki.sagir@ik.me>
2026-01-09 10:42:50 +0100

    Update ct/cobalt.sh
    Co-authored-by: Tobias <96661824+CrazyWolf13@users.noreply.github.com>

--- ct/cobalt.sh
+++ ct/cobalt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
-# Copyright (c) 2021-2025 tteck / Community
-# Author: Community Contributors
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Kofysh
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/imputnet/cobalt
 
