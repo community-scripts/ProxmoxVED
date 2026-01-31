1422676d30e76675f45d4ea1eedc9b836152a999
Jared Vititoe <jjvititoe1@gmail.com>
2026-01-31 14:27:43 -0500

    TEMP: Point to fork for testing (revert before merge)

--- ct/beets.sh
+++ ct/beets.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/JaredVititoe/ProxmoxVED/refs/heads/add-beets-script/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: JaredVititoe (JaredVititoe)
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
