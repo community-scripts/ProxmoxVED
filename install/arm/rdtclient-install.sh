53d84b11feb54f478f9f68bfd1b7782f682032aa
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2026-03-07 15:03:27 +1000

    arm64-rdtclient: remove libicu72 dependency

--- install/arm/rdtclient-install.sh
+++ install/arm/rdtclient-install.sh
@@ -6,15 +6,7 @@ install_dotnet_runtime() {
   msg_ok "Installed Dependencies"
 
   msg_info "Installing ASP.NET Core Runtime"
-  $STD apt-get install -y libc6
-  $STD apt-get install -y libgcc-s1
-  $STD apt-get install -y libgssapi-krb5-2
-  $STD apt-get install -y libicu72 || $STD apt-get install -y libicu76
-  $STD apt-get install -y liblttng-ust1
-  $STD apt-get install -y libssl3
-  $STD apt-get install -y libstdc++6
-  $STD apt-get install -y zlib1g
-
+  $STD apt-get install -y libc6 libgcc-s1 libgssapi-krb5-2 liblttng-ust1 libssl3 libstdc++6 zlib1g libicu76
   curl -fsSL -o /tmp/dotnet.tar.gz "https://download.visualstudio.microsoft.com/download/pr/6f79d99b-dc38-4c44-a549-32329419bb9f/a411ec38fb374e3a4676647b236ba021/dotnet-sdk-9.0.100-linux-arm64.tar.gz"
   mkdir -p /usr/share/dotnet
   tar -zxf /tmp/dotnet.tar.gz -C /usr/share/dotnet
