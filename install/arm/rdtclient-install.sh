20ae91a95c1f9250ba005ff73a02f999ac416bea
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2026-03-07 15:18:51 +1000

    arm64-rdtclient: update dotnet to 10.0

--- install/arm/rdtclient-install.sh
+++ install/arm/rdtclient-install.sh
@@ -7,7 +7,7 @@ install_dotnet_runtime() {
 
   msg_info "Installing ASP.NET Core Runtime"
   $STD apt-get install -y libc6 libgcc-s1 libgssapi-krb5-2 liblttng-ust1 libssl3 libstdc++6 zlib1g libicu76
-  curl -fsSL -o /tmp/dotnet.tar.gz "https://download.visualstudio.microsoft.com/download/pr/6f79d99b-dc38-4c44-a549-32329419bb9f/a411ec38fb374e3a4676647b236ba021/dotnet-sdk-9.0.100-linux-arm64.tar.gz"
+  curl -fsSL -o /tmp/dotnet.tar.gz "https://builds.dotnet.microsoft.com/dotnet/Sdk/10.0.103/dotnet-sdk-10.0.103-linux-arm64.tar.gz"
   mkdir -p /usr/share/dotnet
   tar -zxf /tmp/dotnet.tar.gz -C /usr/share/dotnet
   ln -sf /usr/share/dotnet/dotnet /usr/bin/dotnet
