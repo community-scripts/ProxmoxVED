16e90c08a77e3fd83bd48d00bacab67081bb2157
John Doe <johndoe@example.com>
2026-03-08 23:09:38 -0400

    add gpu passthrough

--- ct/hermes-agent.sh
+++ ct/hermes-agent.sh
@@ -15,6 +15,7 @@ var_disk="${var_disk:-10}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
 var_unprivileged="${var_unprivileged:-1}"
+var_gpu="${var_gpu:-yes}"
 
 header_info "$APP"
 variables
