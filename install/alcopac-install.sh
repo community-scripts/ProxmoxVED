9371eeadf863362c0d2d42b347f6b2ba74615021
Hlushok <101033368+Hlushok@users.noreply.github.com>
2026-03-11 09:26:28 +0200

    Update copyright information and installation method

--- install/alcopac-install.sh
+++ install/alcopac-install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021-2026 tteck
-# Author: tteck (tteckster)
+
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: Hlushok
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://dev.alcopa.cc/
 
@@ -12,15 +13,9 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Base Dependencies"
-$STD apt-get install -y unzip
-msg_ok "Installed Base Dependencies"
-
-msg_info "Installing Alcopac (Non-interactive)"
-TEMP_INSTALL="$(mktemp)"
-trap 'rm -f "$TEMP_INSTALL"' EXIT
-$STD curl -fsSL https://dev.alcopa.cc/install -o "$TEMP_INSTALL"
-$STD bash "$TEMP_INSTALL" install
+msg_info "Installing Alcopac"
+msg_warn "This script will install Alcopac using a third-party installer from dev.alcopa.cc"
+$STD bash <(curl -fsSL https://dev.alcopa.cc/install) install
 msg_ok "Installed Alcopac"
 
 motd_ssh
