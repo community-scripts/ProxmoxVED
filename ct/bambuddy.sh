42ffd777ddb6b22a14150fc1416f6082c038f99b
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2026-03-14 13:03:30 +0100

    typo

--- ct/bambuddy.sh
+++ ct/bambuddy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author:[Adrian-RDA
+# Author: Adrian-RDA
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/maziggy/bambuddy
 
