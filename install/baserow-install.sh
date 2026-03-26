692823fa16008dc8df29288b66a207f7793eb734
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-03-26 08:28:37 +0100

    Update baserow-install.sh

--- install/baserow-install.sh
+++ install/baserow-install.sh
@@ -45,7 +45,7 @@ msg_ok "Installed Backend Dependencies"
 
 msg_info "Building Frontend"
 cd /opt/baserow/web-frontend
-$STD npm install
+$STD npm install --legacy-peer-deps
 $STD npm run build
 msg_ok "Built Frontend"
 
