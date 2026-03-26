afc21cae3462379c80197a169b76858f0f9cf95b
CrazyWolf13 <tobias.meier@edu.tbz.ch>
2026-03-26 15:54:56 +0100

    fix: geopulse: update check

--- ct/geopulse.sh
+++ ct/geopulse.sh
@@ -29,7 +29,7 @@ function update_script() {
     exit
   fi
 
-  if check_for_gh_release "geopulse" "tess1o/geopulse"; then
+  if check_for_gh_release "geopulse-backend" "tess1o/geopulse"; then
     msg_info "Stopping Service"
     systemctl stop geopulse-backend
     msg_ok "Stopped Service"
