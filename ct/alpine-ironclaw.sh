07906eb73e10e96062443166adf0ae41274f202c
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-03-27 13:14:45 +0100

    alpine

--- ct/alpine-ironclaw.sh
+++ ct/alpine-ironclaw.sh
@@ -5,10 +5,10 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/nearai/ironclaw
 
-APP="IronClaw"
+APP="Alpine-IronClaw"
 var_tags="${var_tags:-ai;agent;alpine}"
-var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-2048}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-1024}"
 var_disk="${var_disk:-8}"
 var_os="${var_os:-alpine}"
 var_version="${var_version:-3.23}"
