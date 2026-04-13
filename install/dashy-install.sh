31a7493564e8ebbd96d3c87962304cc9f6b9daa1
Tobias <git@tobiasmeier.dev>
2026-04-13 21:18:35 +0200

    dashy: fix repo

--- install/dashy-install.sh
+++ install/dashy-install.sh
@@ -14,7 +14,7 @@ network_check
 update_os
 
 NODE_VERSION="18" NODE_MODULE="yarn" setup_nodejs
-fetch_and_deploy_gh_release "dashy" "Lissy93/dashy" "prebuild" "latest" "/opt/dashy" "dashy-*.tar.gz"
+fetch_and_deploy_gh_release "dashy" "CrazyWolf13/dashy" "prebuild" "latest" "/opt/dashy" "dashy-*.tar.gz"
 
 msg_info "Installing Dashy"
 cd /opt/dashy
