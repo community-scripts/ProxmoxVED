ebe0dc4e8ed68dd4c7f89b97b2d8b937212e4dc2
nnsense <2553412+nnsense@users.noreply.github.com>
2026-05-04 21:04:28 +0000

    fix: update func

--- ct/pinchflat.sh
+++ ct/pinchflat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-COMMUNITY_SCRIPTS_URL="${COMMUNITY_SCRIPTS_URL:-https://raw.githubusercontent.com/nnsense/ProxmoxVED/main}"
-source <(curl -fsSL "$COMMUNITY_SCRIPTS_URL/misc/build.func")
+
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: nnsense
