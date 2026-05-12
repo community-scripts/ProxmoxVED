344d15a19e0130113f9a088c6a119b387418e134
Ivan <kichoky@gmail.com>
2026-05-12 07:02:35 +0200

    fix: serve ruTorrent at / not /rutorrent (#7)

--- ct/rutorrent.sh
+++ ct/rutorrent.sh
@@ -191,5 +191,5 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}/rutorrent${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}/${CL}"
 echo -e "${INFO}${YW} Web UI credentials are in ${BGN}~/rutorrent.creds${CL} inside the container.${CL}"
