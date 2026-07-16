eee1f2c7b76a69742ccfb8c89732493268cd79c7
MickLesk <mickey.leskowitz@gmail.com>
2026-07-16 14:01:56 +0200

    fix http for jdownloader

--- ct/jdownloader.sh
+++ ct/jdownloader.sh
@@ -48,5 +48,5 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
-echo -e "${INFO}${YW} Manage your downloads remotely via:${CL}"
-echo -e "${GATEWAY}${BGN}https://${IP}:6080/vnc.html?resize=scale&autoconnect=true${CL}"
+echo -e "${INFO}${YW}Access it using the following URL:${CL}"
+echo -e "${GATEWAY}${BGN}http://${IP}:6080/vnc.html?resize=scale&autoconnect=true${CL}"
