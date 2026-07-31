464698a75dbbcd4c8ed90a0bab1527fff50356d6
MickLesk <mickey.leskowitz@gmail.com>
2026-07-31 11:51:23 +0200

    Update logto.sh

--- ct/logto.sh
+++ ct/logto.sh
@@ -69,4 +69,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW}Access it using the following URL:${CL}"
-echo -e "${GATEWAY}${BGN}http://${IP}:3002${CL}"
+echo -e "${GATEWAY}${BGN}https://${IP}:3002${CL}"
