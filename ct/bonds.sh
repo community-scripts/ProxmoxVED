cfef00362fe706b675fe0d5df2d461c23f0ff6f9
root <root@roverdi01.mity.local>
2026-08-04 12:20:29 +0200

    added scripts for bonds

---
+++ ct/bonds.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: community-scripts
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/naiba/bonds | Github: https://github.com/naiba/bonds
+
+APP="Bonds"
+var_tags="${var_tags:-erp}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-512}"
+var_disk="${var_disk:-4}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -f /etc/systemd/system/bonds.service ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  if check_for_gh_release "bonds" "naiba/bonds"; then
+    msg_info "Stopping Service"
+    systemctl stop bonds
+    msg_ok "Stopped Service"
+
+    msg_info "Updating ${APP}"
+    fetch_and_deploy_gh_release "bonds" "naiba/bonds" "singlefile" "latest" "/opt/bonds" "bonds-server"
+    chmod +x /opt/bonds/bonds-server
+    msg_ok "Updated ${APP}"
+
+    msg_info "Starting Service"
+    systemctl start bonds
+    msg_ok "Started Service"
+    msg_ok "Updated successfully!"
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW}Access it using the following URL:${CL}"
+echo -e "${GATEWAY}${BGN}http://${IP}:8080${CL}"
