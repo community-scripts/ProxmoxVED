1d6bc6ca1468dac1e1d265f3e504f7f83869c575
Owen Voke <development@voke.dev>
2026-09-14 20:15:22 +0100

    style: update Razzia to use `nginx_enable_site`

--- install/razzia-install.sh
+++ install/razzia-install.sh
@@ -88,10 +88,7 @@ server {
   }
 }
 EOF
-ln -sf /etc/nginx/sites-available/razzia /etc/nginx/sites-enabled/razzia
-rm -f /etc/nginx/sites-enabled/default
-$STD nginx -t
-systemctl reload nginx
+nginx_enable_site razzia
 msg_ok "Configured Nginx"
 
 motd_ssh
