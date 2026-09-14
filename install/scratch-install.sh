38f33caf53336d1ca64e7c45f8ff0306e17737b3
ma <ma@amatis.de>
2026-09-14 20:56:34 +0200

    Fill in Author line

--- install/scratch-install.sh
+++ install/scratch-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author: <your-github-handle>
+# Author: ma-world
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/scratchfoundation/scratch-editor
 
